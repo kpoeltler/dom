02453943e82ff9f0e7943c89080762b2a1a80d2c
Katie Poeltler <katiepoeltler@Katies-MacBook-Pro.local>
2018-01-23 15:08:49 -0600

    new js

--- library.js
+++ library.js
@@ -1,60 +1,70 @@
 
-var $ = function(sel) {
-  var elem = document.querySelectorAll(sel);
-	
-	var text = function(str) {
-    // elem.innerText=str;
-    for (var i = 0; 1 < elem.length; i++) {
-      elem[i].innerText=(str);
-    }
-  }
+// var $ = function(sel){
+//     var str = "The selector is " + sel
+//     return str;
 
-	var addClass= function (className){
-		for (i = 0; i < elem.length; i++) {
-			elem [i].classList.add (className);
-		}
+// }
+
+var $ = function (sel) {
+
+	var elem = document.querySelectorAll(sel);
+
+	var text = function (str) {
+			// elem.innerText = str;
+			for (var i = 0; i < elem.length; i++) {
+					elem[i].innerText = str;
+			}
 	}
 
-	var removeClass= function (className){
-		for (i = 0; i < elem.length; i++) {
-			elem[i]. classList.remove (className);
-		}
+	var addClass = function (className) {
+			for (var i = 0; i < elem.length; i++) {
+					elem[i].classList.add(className);
+			}
 	}
 
-	var on= function (action, cb) {
-		for (var i =0; i < elem.length; i++) {
-			elem[i].addEventListener(action, cb);
-			
-		}
+	var removeClass = function (className) {
+			for (var i = 0; i < elem.length; i++) {
+					elem[i].classList.remove(className);
+			}
+	}
+
+	var on = function (action, cb) {
+			for (var i = 0; i < elem.length; i++) {
+					elem[i].addEventListener(action, cb);
+			}
+	}
+
+	var html = function (newHTML) {
+			for (var i = 0; i < elem.length; i++) {
+					elem[i].innerHTML = newHTML;
+			}
+	}
+
+	var toggleClass = function (className) {
+			for (var i = 0; i < elem.length; i++) {
+					if (elem[i].className.includes(className)) {
+							elem[i].classList.remove(className);
+					}
+					else {
+							elem[i].classList.add(className);
+					}
+			}
+
+
 	}
 
 
-var toggleClass = function (classname){
-	for (var i = 0; i < elem.length; i++)
-if (elem [i].classList.includes (className)) {
-		elem [i].classList.remove (className);
-} else {
-	elem [i].classList.add (className);
-		}
+
+
+	var publicAPI = {
+			element: elem,
+			text: text,
+			addClass: addClass,
+			removeClass: removeClass,
+			on: on,
+			html: html,
+			toggleClass:toggleClass,
 	}
+
+	return publicAPI;
 }
-
-var html = function (newHTML){
-	 for (var i = 0; 1 < elem.length; i++) {
-		elem[i].innerHTML=newHTML;
-	}
-}
-
-  var publicAPI = {
-    element: elem,
-    text: text,
-    addClass: addClass,
-		removeClass: removeClass,
-		on: on,
-		html : html
-		
-	}
-	
-  return publicAPI;
-
-
